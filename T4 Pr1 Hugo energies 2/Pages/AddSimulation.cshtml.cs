80bf79e7376f5ed3d6bae6844c1bb0381646acda
hresi <hugo.resina.7e7@itb.cat>
2025-03-11 22:28:32 +0100

    save progress

--- T4 Pr1 Hugo energies 2/Pages/AddSimulation.cshtml.cs
+++ T4 Pr1 Hugo energies 2/Pages/AddSimulation.cshtml.cs
@@ -9,39 +9,34 @@ namespace T4_Pr1_Hugo_energies_2.Pages
     public class AddSimulationModel : PageModel
     {
         [BindProperty]
-        public ASystem Simulation { get; set; }
-
-        [BindProperty]
         public string SystemType { get; set; }
 
         [BindProperty]
         public double CalculationParameter { get; set; }
 
+        [BindProperty]
+        public double Ratio { get; set; }
+
         public IActionResult OnPost()
         {
-            if (!ModelState.IsValid)
+            ASystem simulation = SystemType switch
             {
+                "Solar" => new SolarSystem(),
+                "Eolic" => new EolicSystem(),
+                "Hydroelectric" => new HydroelectricSystem(),
+                _ => null
+            };
+
+            if (simulation == null)
+            {
+                ModelState.AddModelError(string.Empty, "Invalid system type.");
                 return Page();
             }
 
-            switch (SystemType)
-            {
-                case "Solar":
-                    Simulation = new SolarSystem();
-                    break;
-                case "Eolic":
-                    Simulation = new EolicSystem();
-                    break;
-                case "Hydroelectric":
-                    Simulation = new HydroelectricSystem();
-                    break;
-            }
-
-            Simulation.Date = DateTime.UtcNow;
-            Simulation.value = CalculationParameter;
-            Simulation.CalculateEnergy();
-            Simulation.totalCost = Simulation.energyGenerated * Simulation.costPerKWh;
-            Simulation.totalPrice = Simulation.energyGenerated * Simulation.pricePerKWh;
+            simulation.Date = DateTime.Now;
+            simulation.value = CalculationParameter;
+            simulation.ratio = Ratio;
+            simulation.CalculateEnergy();
 
             string path = Path.Combine(Directory.GetCurrentDirectory(), "ModelData", "simulacions_energia.csv");
             bool fileExists = System.IO.File.Exists(path);
@@ -49,9 +44,10 @@ namespace T4_Pr1_Hugo_energies_2.Pages
             {
                 if (!fileExists)
                 {
-                    writer.WriteLine("DateGenerated|SystemType|CalculationParameter|Ratio|EnergyGenerated|CostPerKWh|PricePerKWh|TotalCost|TotalPrice");
+                    writer.WriteLine("DateGenerated|SystemType|CalculationParameter|Ratio|EnergyGenerated");
                 }
-                writer.WriteLine($"{Simulation.Date}|{Simulation.SimulationType}|{Simulation.value}|{Simulation.ratio}|{Simulation.energyGenerated}|{Simulation.costPerKWh}|{Simulation.pricePerKWh}|{Simulation.totalCost}|{Simulation.totalPrice}");
+                writer.WriteLine($"{simulation.Date.ToString("yyyy-MM-dd")}|{simulation.SimulationType}|{simulation.value}|{simulation.ratio}|{simulation.energyGenerated}");
+                writer.WriteLine(); // Añadir una nueva línea después de cada entrada
             }
 
             return RedirectToPage("/Simulations");
--- T4 Pr1 Hugo energies 2/Pages/Simulations.cshtml.cs
+++ T4 Pr1 Hugo energies 2/Pages/Simulations.cshtml.cs
@@ -7,49 +7,49 @@ using T4_Pr1_Hugo_energies_2.Model;
 
 namespace T4_Pr1_Hugo_energies_2.Pages
 {
-    public class SimulacionsModel : PageModel
-    {
-        private const string FilePath = @"ModelData\simulacions_energia.csv";
-        public List<ASystem> EnergySystems { get; set; } = new List<ASystem>();
+	public class SimulationsModel : PageModel
+	{
+		private const string FilePath = "ModelData\\simulacions_energia.csv";
+		public List<ASystem> EnergySystems { get; set; } = new List<ASystem>();
 
-        public void OnGet()
-        {
-            if (System.IO.File.Exists(FilePath))
-            {
-                var lines = System.IO.File.ReadAllLines(FilePath);
-                foreach (var line in lines)
-                {
-                    var values = line.Split('|');
-                    if (values.Length < 9) continue; // Evitar errores en líneas inválidas
+		public void OnGet()
+		{
+			if (System.IO.File.Exists(FilePath))
+			{
+				var lines = System.IO.File.ReadAllLines(FilePath);
+				foreach (var line in lines)
+				{
+					var values = line.Split('|');
+					
 
-                    string type = values[1];
-                    DateTime date = DateTime.ParseExact(values[0], "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture);
-                    double parameter = double.Parse(values[2]);
-                    double ratio = double.Parse(values[3]);
-                    double energyGenerated = double.Parse(values[4]);
-                    double costPerKWh = double.Parse(values[5]);
-                    double pricePerKWh = double.Parse(values[6]);
-                    double totalCost = double.Parse(values[7]);
-                    double totalPrice = double.Parse(values[8]);
+					string type = values[1];
+					DateTime date = DateTime.ParseExact(values[0],"yyyy-mm-dd", CultureInfo.InvariantCulture);
+					double parameter = double.Parse(values[2], CultureInfo.InvariantCulture);
+					double ratio = double.Parse(values[3], CultureInfo.InvariantCulture);
+					double energyGenerated = double.Parse(values[4], CultureInfo.InvariantCulture);
+					/*double costPerKWh = double.Parse(values[5], CultureInfo.InvariantCulture);
+					double pricePerKWh = double.Parse(values[6], CultureInfo.InvariantCulture);
+					double totalCost = double.Parse(values[7], CultureInfo.InvariantCulture);
+					double totalPrice = double.Parse(values[8], CultureInfo.InvariantCulture);*/
 
-                    ASystem system = type switch
-                    {
-                        "Solar" => new SolarSystem { Date = date, value = parameter, ratio = ratio, energyGenerated = energyGenerated },
-                        "Eolic" => new EolicSystem { Date = date, value = parameter, ratio = ratio, energyGenerated = energyGenerated },
-                        "Hydro" => new HydroelectricSystem { Date = date, value = parameter, ratio = ratio, energyGenerated = energyGenerated },
-                        _ => null
-                    };
+					ASystem system = type switch
+					{
+						"Solar" => new SolarSystem { Date = date, value = parameter, ratio = ratio, energyGenerated = energyGenerated },
+						"Eolic" => new EolicSystem { Date = date, value = parameter, ratio = ratio, energyGenerated = energyGenerated },
+						"Hydroelectric" => new HydroelectricSystem { Date = date, value = parameter, ratio = ratio, energyGenerated = energyGenerated },
+						_ => null
+					};
 
-                    if (system != null)
-                    {
-                        system.costPerKWh = costPerKWh;
-                        system.pricePerKWh = pricePerKWh;
-                        system.totalCost = totalCost;
-                        system.totalPrice = totalPrice;
-                        EnergySystems.Add(system);
-                    }
-                }
-            }
-        }
-    }
+					/*if (system != null)
+					{
+						system.costPerKWh = costPerKWh;
+						system.pricePerKWh = pricePerKWh;
+						system.totalCost = totalCost;
+						system.totalPrice = totalPrice;
+						EnergySystems.Add(system);
+					}*/
+				}
+			}
+		}
+	}
 }
